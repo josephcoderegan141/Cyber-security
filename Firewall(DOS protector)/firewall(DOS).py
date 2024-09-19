c001b2db15d2857acb9a21eb804acdc33906305b
josephcoderegan141 <160789417+josephcoderegan141@users.noreply.github.com>
2024-09-19 16:52:02 +0100

    Add files via upload

---
+++ Firewall(DOS protector)/firewall(DOS).py
@@ -0,0 +1,45 @@
+#protects against dos attacks
+import os
+import sys
+import time
+from collections import defaultdict
+from scapy.all import sniff, IP
+
+THRESHOLD = 40
+print(f"THRESHOLD: {THRESHOLD}")
+    
+
+def packet_callback(packet):
+    src_ip = packet[IP].src
+    packet_count[src_ip] += 1
+
+    current_time = time.time()
+    time_interval = current_time - start_time[0]
+    
+    if time_interval >= 1:
+        for ip, count in packet_count.items():
+            packet_rate = count / time_interval
+            if packet_rate > THRESHOLD and ip not in blocked_ips:
+                print("Blocking IP: " ,ip, "packet rate: " ,packet_rate)
+                os.system("iptables -A INPUT -s", ip," -j DROP")
+                blocked_ips.add(ip)
+
+        packet_count.clear()
+        start_time[0] = current_time
+
+
+def main():
+    if os.geteuid() != 0:
+        print("This script requires root privileges.")
+        sys.exit(1)
+
+    packet_count = defaultdict(int)
+    start_time = [time.time()]
+    blocked_ips = set()
+
+    print("Monitoring network traffic...")
+    sniff(filter="ip", prn=packet_callback)
+
+
+main()
+
